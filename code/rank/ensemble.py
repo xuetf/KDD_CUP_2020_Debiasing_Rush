4188ec0eb8d32af15b44ffa0aca49c570bd2497f
xuetf <476122294@qq.com>
2020-06-19 21:42:22 +0800

    rank-code-checked

--- code/rank/ensemble.py
+++ code/rank/ensemble.py
@@ -24,19 +24,19 @@ def ensemble(output_ranking_filename):
     lgb_ranker_df = pickle.load(
         open('{}/{}'.format(rank_output_dir, lgb_output_file), 'rb'))
     lgb_ranker_df['sim'] = lgb_ranker_df.groupby('user_id')['sim'].transform(lambda df: norm_sim(df))
-
+    print('read lgb done...')
     # read din
     din_output_file = 'din-' + output_ranking_filename + '.csv-pkl'
     din_df = pickle.load(
         open('{}/{}'.format(rank_output_dir, din_output_file), 'rb'))
     din_df['sim'] = din_df.groupby('user_id')['sim'].transform(lambda df: norm_sim(df))
-
+    print('read din done...')
     # fuse lgb and din
     din_lgb_full_df = lgb_ranker_df.append(din_df)
     din_lgb_full_df = din_lgb_full_df.groupby(['user_id', 'item_id', 'phase'])['sim'].sum().reset_index()
 
-    online_top50_click_np, online_top50_click = obtain_top_k_click()
-    res3 = get_predict(din_lgb_full_df, 'sim', online_top50_click)
+    _, top50_click = obtain_top_k_click()
+    res3 = get_predict(din_lgb_full_df, 'sim', top50_click)
     res3.to_csv(output_path + '/result.csv', index=False, header=None)
 
 
