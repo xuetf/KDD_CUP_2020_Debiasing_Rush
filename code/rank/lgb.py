5c6aa5a4b9cd088de58d64530e0d23aaca44df5d
xuetf <476122294@qq.com>
2020-07-05 13:56:31 +0800

    lgb

--- code/rank/lgb.py
+++ code/rank/lgb.py
@@ -13,7 +13,7 @@ def lgb_main(train_final_df, val_final_df=None):
 
     lgb_ranker = lgb.LGBMRanker(
         boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
-        max_depth=-1, n_estimators=300, objective='binary',
+        max_depth=-1, n_estimators=300,
         subsample=0.7, colsample_bytree=0.7, subsample_freq=1,
         learning_rate=0.01, min_child_weight=50, random_state=2018,
         n_jobs=-1)  # 300epoch, best, 0.882898, dense_feat  + hist_cnt_sim_feat user_interest_dense_feat
